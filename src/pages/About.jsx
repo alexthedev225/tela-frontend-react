89ab01455229de22bdbe7055bfea348fe54ea99e
alexthedev225 <alexcode225@gmail.com>
2023-10-31 22:24:39 +0100

    Amélioration de la section maison a louer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,34 +1,50 @@
+import { useState } from "react";
 import RootLayout from "../Layout";
-import '../styles/About.css'
+import "../styles/About.css";
 
 function About() {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal); // Inverse l'état de la boîte modale (ouvrir/fermer)
+  };
+
   return (
     <RootLayout>
-      <p style={{color: 'white'}}>
+      <p style={{ color: "white" }}>
         Dans le souci de participer activement au développement de notre pays et
-        surtout faciliter la vie aux populations, nous n’avons de cesse de
+        surtout faciliter la vie aux populations, nous n&apos;avons de cesse de
         réflechir et apporter notre contribution pour impacter positivement la
-        vie de chaque citoyen vivant sur le sol d’Eburnie. <br /> C’est dans cette
-        optique que nous vous proposons la plateforme TELA. TELA est une
+        vie de chaque citoyen vivant sur le sol d&apos;Eburnie. <br /> C&apos;est dans
+        cette optique que nous vous proposons la plateforme TELA. TELA est une
         application tout en un qui a été mise en place dans le but de faciliter
-        la vie aux populations vivant en Côte d’Ivoire. <br /><br /> Premièrement, avec Tela,
-        désormais c’est facile de trouver rapidement un logement à son gout.
-        Pour y arriver, il suffit de cliquer dans la rubrique “Maison à louer”
-        pour trouver le logement de son choix. <br /> De plus c’est une application qui
-        va générer à terme plus de 30 000 emplois sur toute l’étendue du
-        territoire ivoirien. A ce niveau il s’agit pur le citoyen lambda de
-        transférer des photos de maisons à sur la plateforme pour gagner au
-        moins 150 000 FCFA chaque fin de mois. (Voir conditions ICI pour devenir
-        démarcheur Tela) <br /> <br /> Deuxièmement, le démarcheur après avoir reçu son
-        virement mensuel peut décider d’épargner sur la plateforme tout en se
-        conformant aux règles de transfère de photos et autres conditions qui
-        seront précisées pour voir son épargne majorée d’un bonus périodique. <br /><br />
-        Troisièmement l’application comporte une web TV dénommée TELA TV, “La
+        la vie aux populations vivant en Côte d&apos;Ivoire. <br />
+        <br /> Premièrement, avec Tela, désormais c&apos;est facile de trouver
+        rapidement un logement à son gout. Pour y arriver, il suffit de cliquer
+        dans la rubrique “Maison à louer” pour trouver le logement de son choix.{" "}
+        <br /> De plus c&apos;est une application qui va générer à terme plus de 30
+        000 emplois sur toute l&apos;étendue du territoire ivoirien. A ce niveau il
+        s&apos;agit pur le citoyen lambda de transférer des photos de maisons à sur
+        la plateforme pour gagner au moins 150 000 FCFA chaque fin de mois.
+        (Voir conditions{" "}
+        <span
+          style={{ textDecoration: "underline", cursor: "pointer" }}
+          onClick={toggleModal}
+        >
+          ICI
+        </span>{" "}
+        pour devenir démarcheur Tela) <br /> <br /> Deuxièmement, le démarcheur
+        après avoir reçu son virement mensuel peut décider d&apos;épargner sur la
+        plateforme tout en se conformant aux règles de transfère de photos et
+        autres conditions qui seront précisées pour voir son épargne majorée
+        d&apos;un bonus périodique. <br />
+        <br />
+        Troisièmement l&apos;application comporte une web TV dénommée TELA TV, “La
         meilleure” , qui est une vitrine pour les petites et moyennes
         entreprises, les petits et moyens commerces mais aussi et surtout pour
         la jeunesse du pays tout entier. Ses émissions en Live vous permettront
         de vous distraire sainement et très souvent de vous faire gagner des
-        lots et de l’argent en participant directement sur le plateau de
+        lots et de l&apos;argent en participant directement sur le plateau de
         diffusion ou en ligne. TELA TV accordera une place spéciale à la CAN
         2023 qui se déroulera dans notre pays à travers des reportages sportifs,
         des émissions sportives en direct et probablement la diffusion en Live
@@ -36,7 +52,14 @@ function About() {
         coulisses des stades pour prendre la températures avec les fans
         ivoiriens et étrangers.{" "}
       </p>
-      </RootLayout>
+      {showModal && (
+        <div className="modal-container" onClick={toggleModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <img src="/condition.JPG" alt="condition pour devenir demarcheur" />
+          </div>
+        </div>
+      )}
+    </RootLayout>
   );
 }
 
--- src/pages/Maison.jsx
+++ src/pages/Maison.jsx
@@ -1,25 +1,302 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import RootLayout from "../Layout";
 import "../styles/Maison.css";
 
 export default function Maison() {
+  const [zone, setZone] = useState("");
+  const [showOptions, setShowOptions] = useState(false);
+  const [showTable, setShowTable] = useState(false);
+  const [formData, setFormData] = useState({
+    commune: "",
+    nombrePieces: "",
+    nombreSallesDeau: "",
+    typeMaisons: "",
+    typeHabitat: "",
+    cour: [],
+    gardien: "",
+    garage: "",
+  });
+
+  const showOptionsContent = () => {
+    setShowOptions(true);
+  };
+
+  const showTableContent = () => {
+    setShowTable(true);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    const updatedData = { ...formData };
+
+    if (type === "checkbox") {
+      if (checked) {
+        updatedData[name] = [...formData[name], value];
+      } else {
+        updatedData[name] = formData[name].filter((item) => item !== value);
+      }
+    } else if (type === "radio") {
+      updatedData[name] = value;
+    } else {
+      updatedData[name] = value;
+    }
+
+    setFormData(updatedData);
+  };
+
+  const performSearch = () => {
+    // Envoyer les données au backend pour effectuer la recherche
+    // Vous devrez implémenter cette partie côté backend
+    console.log("Formulaire soumis avec les données suivantes :");
+    console.log(formData);
+  };
+
   return (
     <RootLayout>
-        
-      <Link to="/maison" className="maison-link">
-      <p className="maison-text">Maison A <br />louer</p>
-        <img
-          src="/Maison1.jpg"
-          alt="façade de la première maison"
-          className="maison-image-1" // Classe pour la première image
-        />
-        <img
-          src="/Maison2.jpg"
-          alt="façade de la deuxième maison"
-          style={{ marginTop: "4rem" }}
-          className="maison-image-2" // Classe pour la deuxième image
-        />
-      </Link>
+      {showTable ? (
+        <div className="table-container">
+          <table>
+            <tbody>
+              <tr>
+                <td>Commune</td>
+                <td>
+                  <select
+                    name="commune"
+                    value={formData.commune}
+                    onChange={handleInputChange}
+                  >
+                    <option value="" disabled></option>
+                    <option value="Abobo">Abobo</option>
+                    <option value="Adjamé">Adjamé</option>
+                    <option value="Anyama">Anyama</option>
+                    <option value="Attécoubé">Attécoubé</option>
+                    <option value="Bingerville">Bingerville</option>
+                    <option value="Cocody">Cocody</option>
+                    <option value="Koumassi">Koumassi</option>
+                    <option value="Marcory">Marcory</option>
+                    <option value="Plateau">Plateau</option>
+                    <option value="Port bouët">Port bouët</option>
+                    <option value="Treichville">Treichville</option>
+                    <option value="Songon">Songon</option>
+                    <option value="Yopougon">Yopougon</option>
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <td>Préciser la zone</td>
+                <td>
+                  <input
+                    type="text"
+                    name="zone"
+                    value={zone}
+                    onChange={(e) => setZone(e.target.value)}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>MAISON BASSE</td>
+                <td>
+                  <input
+                    type="radio"
+                    name="typeMaisons"
+                    value="MAISON BASSE"
+                    checked={formData.typeMaisons === "MAISON BASSE"}
+                    onChange={handleInputChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>APPARTEMENT</td>
+                <td>
+                  <input
+                    type="radio"
+                    name="typeMaisons"
+                    value="APPARTEMENT"
+                    checked={formData.typeMaisons === "APPARTEMENT"}
+                    onChange={handleInputChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>DUPLEX</td>
+                <td>
+                  <input
+                    type="radio"
+                    name="typeMaisons"
+                    value="DUPLEX"
+                    checked={formData.typeMaisons === "DUPLEX"}
+                    onChange={handleInputChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>HABITAT HAUT STANDING AVEC PISCINE</td>
+                <td>
+                  <input
+                    type="radio"
+                    name="typeHabitat"
+                    value="HABITAT HAUT STANDING AVEC PISCINE"
+                    checked={formData.typeHabitat === "HABITAT HAUT STANDING AVEC PISCINE"}
+                    onChange={handleInputChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>HABITAT HAUT STANDING SANS PISCINE</td>
+                <td>
+                  <input
+                    type="radio"
+                    name="typeHabitat"
+                    value="HABITAT HAUT STANDING SANS PISCINE"
+                    checked={formData.typeHabitat === "HABITAT HAUT STANDING SANS PISCINE"}
+                    onChange={handleInputChange}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>NOMBRE DE PIECES</td>
+                <td>
+                  <select
+                    name="nombrePieces"
+                    value={formData.nombrePieces}
+                    onChange={handleInputChange}
+                  >
+                    <option value="" disabled></option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                  </select>
+                </td>
+              </tr>
+              <tr>
+                <td>NOMBRE DE SALLE D&apos;EAU</td>
+                <td>
+                  <select
+                    name="nombreSallesDeau"
+                    value={formData.nombreSallesDeau}
+                    onChange={handleInputChange}
+                  >
+                    <option value="" disabled></option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table>
+            <tr>
+              <td>COMMODITES ADDITIONNELLES</td>
+              <td>Ligne 1, Colonne 2</td>
+            </tr>
+            <tr>
+              <td>COUR AVANT</td>
+              <td>
+                <input
+                  type="checkbox"
+                  name="cour"
+                  value="COUR AVANT"
+                  checked={formData.cour.includes("COUR AVANT")}
+                  onChange={handleInputChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>COUR ARRIERE</td>
+              <td>
+                <input
+                  type="checkbox"
+                  name="cour"
+                  value="COUR ARRIERE"
+                  checked={formData.cour.includes("COUR ARRIERE")}
+                  onChange={handleInputChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>AVEC GARDIEN</td>
+              <td>
+                <input
+                  type="radio"
+                  name="gardien"
+                  value="AVEC GARDIEN"
+                  checked={formData.gardien === "AVEC GARDIEN"}
+                  onChange={handleInputChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>SANS GARDIEN</td>
+              <td>
+                <input
+                  type="radio"
+                  name="gardien"
+                  value="SANS GARDIEN"
+                  checked={formData.gardien === "SANS GARDIEN"}
+                  onChange={handleInputChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>AVEC GARAGE</td>
+              <td>
+                <input
+                  type="radio"
+                  name="garage"
+                  value="AVEC GARAGE"
+                  checked={formData.garage === "AVEC GARAGE"}
+                  onChange={handleInputChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>SANS GARAGE</td>
+              <td>
+                <input
+                  type="radio"
+                  name="garage"
+                  value="SANS GARAGE"
+                  checked={formData.garage === "SANS GARAGE"}
+                  onChange={handleInputChange}
+                />
+              </td>
+            </tr>
+          </table>
+          <button onClick={performSearch}>Valider</button>
+        </div>
+      ) : showOptions ? (
+        <div className="options-container">
+          <button className="location-button" onClick={showTableContent}>
+            Logement
+          </button>
+          <button className="location-button" onClick={showTableContent}>
+            Bureau
+          </button>
+        </div>
+      ) : (
+        <div className="maison-link" onClick={showOptionsContent}>
+          <p className="maison-text">Maison A louer</p>
+          <img
+            src="/Maison1.jpg"
+            alt="façade de la première maison"
+            className="maison-image-1"
+          />
+          <img
+            src="/Maison2.jpg"
+            alt="façade de la deuxième maison"
+            style={{ marginTop: "4rem" }}
+            className="maison-image-2"
+          />
+        </div>
+      )}
     </RootLayout>
   );
 }
